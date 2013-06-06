832a861e98c316cc16594ca923375d3cfca7462f
Alexandre De Dommelin <adedommelin@tuxz.net>
2013-06-06 22:07:32 +0200

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,61 @@
+# just to be able to test my module using puppet apply
+class { 'supervisord': }
+
+
+# == Class: supervisord
+#
+# Manage supervisord
+#
+# === Examples
+#
+#  class { supervisord: }
+#
+# === Authors
+#
+# Alexandre De Dommelin <adedommelin@tuxz.net>
+#
+# === Copyright
+#
+# Copyright 2013 Alexandre De Dommelin
+#
+class supervisord {
+  package { 'supervisor':
+    ensure => present
+  }
+
+  case $::operatingsystem {
+    centos: {
+      $supervisord_config = '/etc/supervisord.conf'
+    }
+    redhat: {
+      $supervisord_config = '/etc/supervisord.conf'
+    }
+    debian: {
+      $supervisord_config = '/etc/supervisor/supervisord.conf'
+    }
+    default: {
+      fail("Module ${module_name} is not supported on ${::operatingsystem}")
+    }
+  }
+
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644'
+  }
+
+  file { '/etc/supervisord':
+    ensure  => 'directory',
+  } -> file { '/etc/supervisord/conf.d':
+    ensure  => 'directory'
+  } -> file { $supervisord_config:
+    source  => 'puppet:///supervisord/supervisord.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644'
+  } -> service { 'supervisor':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['supervisor']
+  }
+}
---
+++ manifests/program.pp
@@ -0,0 +1,44 @@
+#
+# Definition: supervisord::program
+#
+define supervisord::program (
+  $name,
+  $command,
+  $process_name             = '%(program_name)s',
+  $numprocs                 = '1',
+  $directory                = '',
+  $umask                    = '022',
+  $priority                 = '999',
+  $autostart                = true,
+  $autorestart              = true,
+  $startsecs                = '1',
+  $startretries             = '3',
+  $exitcodes                = '0,2',
+  $stopsignal               = 'TERM',
+  $stopwaitsecs             = '10',
+  $user                     = 'root',
+  $redirect_stderr          = false,
+  $stdout_logfile           = 'AUTO',
+  $stdout_logfile_maxbytes  = '50MB',
+  $stdout_logfile_backups   = '10',
+  $stdout_capture_maxbytes  = '0',
+  $stdout_events_enabled    = false,
+  $stderr_logfile           = 'AUTO',
+  $stderr_logfile_maxbytes  = '50MB',
+  $stderr_logfile_backups   = '10',
+  $stderr_capture_maxbytes  = '0',
+  $stderr_events_enabled    = false,
+  $environment              = '',
+  $serverurl                = 'AUTO'
+) {
+
+  file { "${name}.conf":
+    ensure  => 'present',
+    content => template('supervisord_app.conf.erb'),
+    path    => '/etc/supervisor/conf.d',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Class['supervisord']
+  }
+}
